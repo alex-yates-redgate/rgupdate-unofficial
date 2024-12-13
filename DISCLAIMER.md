0434f9594f0acef5f28db3d1575631eda2082ecf
alex-yates-redgate <alex.yates@red-gate.com>
2024-12-13 19:47:37 +0000

    Initial commit: DISCLAIMER

---
+++ DISCLAIMER.md
@@ -0,0 +1,6 @@
+# Disclaimer
+This is not an official Redgate tool, and it is not managed or supported by Redgate. 
+This is a community project, and is provided as-is. If you plan to use it, you should review the code and test it thoroughly before using it.
+Redgate will not provide support for this tool, nor will it be held responsible for it.
+If you have any questions, please contact the author of the software:
+Alex Yates, alex.yates@red-gate.com
---
+++ Program.cs
@@ -0,0 +1,3 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("This is NOT an official Redgate CLI. It is a utility built by Alex Yates to simplify the installation and management of various Redgate CLI tools. By using this software, you confirm that you have read and understand this disclaimer: https://github.com/alex-yates-redgate/rgupdate-unofficial/blob/main/DISCLAIMER.md");
+Console.WriteLine("To do: EVERYTHING!");
